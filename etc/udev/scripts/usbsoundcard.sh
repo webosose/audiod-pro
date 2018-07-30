76427653721d2c28064a9481c1ca3dff109ee064
Velicheti Sita <velicheti.sita@lge.com>
2018-07-30 14:05:23 +0530

    Support external USB sound card during bootup
    :Release Notes:
    Add functionality to detect external USB sound card during bootup
    
    :Detailed Notes:
    Enhance UDEV rules to detect attached external sound cards during
    bootup to support the USB sound card functionality
    
    :Testing Performed:
    Built and tested on local webosose build and GPVB build for Rpi
    
    Connect USB soundcard to the Rpi device and power on the device
    SSH to the device and execute "pactl list modules short" command
    
    Result:
    module-alsa-source device=hw:0,0 mmap=0 source_name=pcm_input
    fragment_size=4096 tsched=0
    module-alsa-sink device=hw:0,0 mmap=0 sink_name=pcm_output
    fragment_size=4096 tsched=0
    
    :QA Notes:
    Verified audio functionality
    
    :Issues Addressed:
    [PLAT-63909] [OSE] Boot time recognition of external USB sound card
    
    Change-Id: Ibe567b1406de3e59149bfbb29d73a8a54f618541

--- etc/udev/scripts/usbsoundcard.sh
+++ etc/udev/scripts/usbsoundcard.sh
@@ -3,10 +3,12 @@ devpath=$2
 device_name=${devpath##*/}
 cardno=${device_name:4:1}
 deviceno=${device_name:6:1}
-until pids=$(pidof pulseaudio)
+until [ $(pidof audiod) > /dev/null ] && [ $(pidof pulseaudio) > /dev/null ]
 do
 sleep 1
 done
+#Waiting for the ls-hub to come up for the below commands.
+sleep 3
 if [ $1 == "USB_MIC_ADDED" ];then
 luna-send -n 1 luna://com.webos.service.audio/udev/event '{"event":"usb-mic-inserted", "soundcard_no":'$cardno', "device_no":'$deviceno'}'
 elif [ $1 == "USB_MIC_REMOVED" ];then
