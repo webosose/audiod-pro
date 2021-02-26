3495b452f0d7814c165660df2d43ccff32f73e1d
Balamurugan Krishnan <balamurugan.krishnan@lge.com>
2021-02-26 14:12:47 +0000

    Implement events header file for module interface design
    :Release Notes:
    Add events header file for module interface design
    
    :Detailed Notes:
    Implementation of adding events header file for module interface
    design to support inter module communication
    
    :Testing Performed:
    Tested on RPI4 board rebased on OSE build #311
    Test cases: QWA-4099
    Test results are attached to PLAT-137284
    
    :QA Notes:
    NA
    
    :Issues Addressed:
    [PLAT-137284] Implement events header file for module interface
                  design support
    
    Change-Id: Ib804a6e4be94e5280a84944196c315511aaa7c27
    Reviewed-on: http://gpro.lge.com/c/webosose/audiod-pro/+/305263
    Reviewed-by: Commit Msg Checker <commit_msg@lge.com>
    Reviewed-by: Ban Word Checker <ban_word@lge.com>
    Reviewed-by: balamurugan krishnan <balamurugan.krishnan@lge.com>
    Reviewed-by: aleti.nageshwar <aleti.nageshwar@lge.com>
    Reviewed-by: <thomas.georgec@lge.com>
    Reviewed-by: Velicheti Sita <velicheti.sita@lge.com>
    Reviewed-by: Mallikarjun H <mallikarjun.h@lge.com>
    Reviewed-by: <chitumodu.yashwanth@lge.com>
    Reviewed-by: Sushovan Gangopadhy <sushovan.g@lge.com>
    Tested-by: balamurugan krishnan <balamurugan.krishnan@lge.com>
    Reviewed-on: http://gpro.lge.com/c/webosose/audiod-pro/+/308464
    Tested-by: <arjit.tyagi@lge.com>
    Reviewed-by: <arjit.tyagi@lge.com>

---
+++ include/events.h
@@ -0,0 +1,92 @@
+// Copyright (c) 2021 LG Electronics, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+
+#ifndef _EVENTS_H_
+#define _EVENTS_H_
+
+#include "utils.h"
+
+namespace events
+{
+    typedef struct
+    {
+        utils::EVENT_TYPE_E eventName;
+        std::string source;
+        std::string sink;
+        EVirtualAudioSink audioSink;
+        utils::ESINK_STATUS sinkStatus;
+        utils::EMIXER_TYPE mixerType;
+    }EVENT_SINK_STATUS_T;
+
+    typedef struct
+    {
+        utils::EVENT_TYPE_E eventName;
+        LUNA_KEY_TYPE_E type;
+        LSMessage *message;
+    }EVENT_KEY_INFO_T;
+
+    typedef struct
+    {
+        utils::EVENT_TYPE_E eventName;
+        LUNA_KEY_TYPE_E type;
+        SERVER_TYPE_E serviceName;
+        std::string api;
+        std::string payload;
+    }EVENT_SUBSCRIBE_KEY_T;
+
+    typedef struct
+    {
+        utils::EVENT_TYPE_E eventName;
+        SERVER_TYPE_E serviceName;
+        bool connectionStatus;
+    }EVENT_SERVER_STATUS_INFO_T;
+
+    typedef struct
+    {
+        utils::EVENT_TYPE_E eventName;
+        bool mixerStatus;
+        utils::EMIXER_TYPE mixerType;
+    }EVENT_MIXER_STATUS_T;
+
+    typedef struct
+    {
+        utils::EVENT_TYPE_E eventName;
+        EVirtualAudioSink audioSink;
+        int volume;
+        bool ramp;
+    }EVENT_INPUT_VOLUME_T;
+
+    typedef struct
+    {
+        //nothing
+        utils::EVENT_TYPE_E eventName;
+    }EVENT_MASTER_VOLUME_STATUS_T;
+
+    typedef struct
+    {
+        utils::EVENT_TYPE_E eventName;
+        SERVER_TYPE_E serviceName;
+    }EVENT_SUBSCRIBE_SERVER_STATUS_T;
+
+
+    typedef struct
+    {
+        utils::EVENT_TYPE_E eventName;
+    }EVENTS_T;
+}
+
+#endif //_EVENTS_H_
