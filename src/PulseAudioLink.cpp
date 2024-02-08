4b84f1fc80457afab57e182e363d48c6c012e785
vaibhav.lawande <vaibhav.lawande@lge.com>
2024-02-08 12:14:04 +0530

    Fix static issue for audiod
    :Release Notes:
    Implement fix for static issue
    
    :Detailed Notes:
    Addressed the following static analysis issue
    RESOURCE_LEAK
    
    :Testing Performed:
    Tested on OSE rebased on latest build #698
    Test case: QWTC-9073, QWTC-428
    
    :QA Notes:
    N/A
    
    :Issues Addressed:
    [CID-6432450] RESOURCE_LEAK
    
    Change-Id: I5ae0339876070fa725883ee858fc8b0b09cec741
    Reviewed-on: http://gpro.lge.com/c/webosose/audiod-pro/+/391709
    Reviewed-by: Sushovan Gangopadhyay <sushovan.g@lge.com>
    Tested-by: Sushovan Gangopadhyay <sushovan.g@lge.com>

--- src/PulseAudioLink.cpp
+++ src/PulseAudioLink.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2012-2022 LG Electronics, Inc.
+// Copyright (c) 2012-2024 LG Electronics, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -564,6 +564,14 @@ void PulseAudioLink::preload(const char * samplename, const char * format, int r
     {
         PM_LOG_ERROR(MSGID_PULSE_LINK, INIT_KVCOUNT, \
                 "fstat Operation failed returning from here");
+        if (fclose(f))
+        {
+            PM_LOG_DEBUG("PulseAudioLink::preload: Failed to close the file");
+        }
+        else
+        {
+            PM_LOG_DEBUG("PulseAudioLink::preload: Successfully closed the file");
+        }
         return;
     }
 
